e013a87bf9e89a1fc28d306f5370f3645624009e
AndryKovalenko <andry.kovalenko@gmail.com>
2018-11-23 19:00:01 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 console.log('Hello!');
 console.log(`The time is ${new Date()}`);
 import './scss/main.scss';
+
+
+
+console.log(`The time is ${new Date()}`);
+
+require('./modules/hello')('NIT');
+var _foo = require('./modules/hello');
+_foo('user1');
+_foo('user2');
+
+// import 'bootstrap';	// with JS!!
+// import 'bootstrap/dist/css/bootstrap.min.css';	// only minified CSS
+
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+
+let _makeProduct = require('./modules/product-html');
+
+jQuery.ajax({
+	url: 'https://nit.tron.net.ua/api/product/list',
+	method: 'get',
+	dataType: 'json',
+	success: function(json){
+		console.log('Loaded via AJAX!');
+		// console.log(json);
+		console.table(json);
+
+		json.forEach(product => $('.product-grid').append(_makeProduct(product)));
+		console.log('Added to grid');
+	},
+	error: function(xhr){
+		alert("An error occured: " + xhr.status + " " + xhr.statusText);
+	},
+});
