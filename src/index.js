f44f764d90b390d02007df9b4972f9957f5eb071
AndryKovalenko <andry.kovalenko@gmail.com>
2018-11-26 14:25:05 +0200

    Poka rabotaet

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ console.log('Hello!');
 console.log(`The time is ${new Date()}`);
 import './scss/main.scss';
 
-
+function validForm(f) {
+    if(f==1){
+        document.getElementsByClassName("row product-grid").$('li:eq(1)').style.display = "none";
+        // document.getElementById("1").style.display = "none";
+        console.log("apple");
+    }
+    console.log("Done");
+}
 
 console.log(`The time is ${new Date()}`);
 
@@ -18,8 +25,9 @@ import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 
+let _productBig = require('./modules/productBig');
 let _makeProduct = require('./modules/product-html');
-
+var products = [];
 jQuery.ajax({
 	url: 'https://nit.tron.net.ua/api/product/list',
 	method: 'get',
@@ -30,9 +38,70 @@ jQuery.ajax({
 		console.table(json);
 
 		json.forEach(product => $('.product-grid').append(_makeProduct(product)));
+        json.forEach(product => $('.single').append(_productBig(product)));
 		console.log('Added to grid');
+
+        // $.each(json, function(id){
+        //     products.push('<li id="' + id + '">' + id + '</li>');
+        // });
+        // for (var i = 0; i < products.length; i++) {
+        //     var counter = products[i];
+        //     console.log(counter);
+        // }
+
 	},
 	error: function(xhr){
 		alert("An error occured: " + xhr.status + " " + xhr.statusText);
 	},
 });
+
+
+// jQuery.ajax({
+//     url: 'https://nit.tron.net.ua/api/product/list',
+//     method: 'get',
+//     dataType: 'json',
+//     success: function(json){
+//         // console.log('Loaded via AJAX!');
+//         // // console.log(json);
+//         // console.table(json);
+//
+//         json.forEach(product => $('.single').append(_productBig(product)));
+//         console.log('Added to grid');
+//
+//         // $.each(json, function(id){
+//         //     products.push('<li id="' + id + '">' + id + '</li>');
+//         // });
+//         // for (var i = 0; i < products.length; i++) {
+//         //     var counter = products[i];
+//         //     console.log(counter);
+//         // }
+//
+//     },
+//     error: function(xhr){
+//         alert("An error occured: " + xhr.status + " " + xhr.statusText);
+//     },
+// });
+
+
+// var filter=[];
+// jQuery.ajax({
+//     url: 'https://nit.tron.net.ua/api/category/list',
+//     method: 'get',
+//     dataType: 'json',
+//     success: function(json){
+//         console.log('Loaded via AJAX!');
+//         // console.log(json);
+//         console.table(json);
+//
+//         $.each(json, function(id,name){
+//             // products.push('<li id="' + id + '">' + id + '</li>');
+//             filter.push('<label ><input id="' + id + '" type="checkbox" name="'+name+'" value="'+name+'">'+name+'</label>');
+//         });
+//
+//     },
+//     error: function(xhr){
+//         alert("An error occured: " + xhr.status + " " + xhr.statusText);
+//     },
+// });
+
+
--- src/modules/productBig.js
+++ src/modules/productBig.js
@@ -1,5 +1,4 @@
-let _makeHtml;
-_makeHtml = ({
+let _bigProduct = ({
                  id,
                  name,
                  image_url,
@@ -7,21 +6,22 @@ _makeHtml = ({
                  price,
                  special_price,
              }) => {
-    let $product = $(`<li class="card col-xs-12 col-sm-4 col-md-3" data-product-id="${id}" id="${id}">`);
+    let $product = $(`<div class="bigProd" data-product-id="${id}" id="b+${id}">`);
     // $product = $(`<a href="" class="single-view" data-product-id="${id}">`);
-    $product.append($(`<img src="${image_url}" alt="${name}" class="img-fluid product-image">`));
+    $product.append($(`<img src="${image_url}" alt="${name}" class="">`));
     $product.append($(`<span class="product-title">`).text(name));
     if(special_price!=null){
         $product.append($(`<span class="price-no">`).text(price));
         $product.append($(`<span>`).text(special_price));
-	}
-	else {
+    }
+    else {
         $product.append($(`<span>`).text(price));
     }
-    // $product.append($(`<span>`).text(description));
-    $product.append($(`<button class="prod+${id}" id="buy" onclick="single(${id})">More</button>`));
+    $product.append($(`<span>`).text(description));
+    $product.append($(`<button class="prod+${id}" id="buyBot" onclick="buy(${id})">Buy</button>`));
+    $product.append($(`<button class="back" onclick="back(${id})">Back</button>`));
     return $product;
 };
 
-module.exports = _makeHtml;
+module.exports = _bigProduct;
 
